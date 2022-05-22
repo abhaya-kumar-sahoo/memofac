6569ef1dda0edcc076ecdca800723c0f5e23e6e1
abhaya-kumar-sahoo <abhayasahoolk@gmail.com>
2022-05-22 17:55:33 +0530

    upadted

--- src/pages/components/MobileImages.js
+++ src/pages/components/MobileImages.js
@@ -1,425 +1,425 @@
-import AmusementPark from "../../assets/photos/AmusementPark-min.png";
-import Apps from "../../assets/photos/Apps-min.png";
-import Aquarium from "../../assets/photos/Aquarium-min.png";
-import Bakeries from "../../assets/photos/Bakeries-min.png";
-import Banks from "../../assets/photos/Banks-min.png";
-import Beach from "../../assets/photos/Beach-min.png";
-import Beer from "../../assets/photos/Beer-min.png";
-import Bicycle from "../../assets/photos/Bicycle-min.png";
+// import AmusementPark from "../../assets/photos/AmusementPark-min.png";
+// import Apps from "../../assets/photos/Apps-min.png";
+// import Aquarium from "../../assets/photos/Aquarium-min.png";
+// import Bakeries from "../../assets/photos/Bakeries-min.png";
+// import Banks from "../../assets/photos/Banks-min.png";
+// import Beach from "../../assets/photos/Beach-min.png";
+// import Beer from "../../assets/photos/Beer-min.png";
+// import Bicycle from "../../assets/photos/Bicycle-min.png";
 
-import Biscuit from "../../assets/photos/Biscuit-min.png";
-import Books from "../../assets/photos/Books-min.png";
-import Camera from "../../assets/photos/Camera-min.png";
-import Candy from "../../assets/photos/Candy-min.png";
-import Car from "../../assets/photos/Car-min.png";
-import Chips from "../../assets/photos/Chips-min.png";
-import Clothes from "../../assets/photos/Clothes-min.png";
-import Coffee from "../../assets/photos/Coffee-min.png";
+// import Biscuit from "../../assets/photos/Biscuit-min.png";
+// import Books from "../../assets/photos/Books-min.png";
+// import Camera from "../../assets/photos/Camera-min.png";
+// import Candy from "../../assets/photos/Candy-min.png";
+// import Car from "../../assets/photos/Car-min.png";
+// import Chips from "../../assets/photos/Chips-min.png";
+// import Clothes from "../../assets/photos/Clothes-min.png";
+// import Coffee from "../../assets/photos/Coffee-min.png";
 
-import ColdDrinks from "../../assets/photos/ColdDrinks-min.png";
-import College from "../../assets/photos/College-min.png";
-import Cosmetics from "../../assets/photos/Cosmetics-min.png";
-import Cruise from "../../assets/photos/Cruise-min.png";
-import CutlerySet from "../../assets/photos/CutlerySet-min.png";
-import Decorators from "../../assets/photos/Decorators-min.png";
-import DrumKit from "../../assets/photos/DrumKit-min.png";
-import EventOrganizer from "../../assets/photos/EventOrganizer-min.png";
+// import ColdDrinks from "../../assets/photos/ColdDrinks-min.png";
+// import College from "../../assets/photos/College-min.png";
+// import Cosmetics from "../../assets/photos/Cosmetics-min.png";
+// import Cruise from "../../assets/photos/Cruise-min.png";
+// import CutlerySet from "../../assets/photos/CutlerySet-min.png";
+// import Decorators from "../../assets/photos/Decorators-min.png";
+// import DrumKit from "../../assets/photos/DrumKit-min.png";
+// import EventOrganizer from "../../assets/photos/EventOrganizer-min.png";
 
-import Flute from "../../assets/photos/Flute-min.png";
-import Footwear from "../../assets/photos/Footwear-min.png";
-import Furniture from "../../assets/photos/Furniture-min.png";
-import Guitar from "../../assets/photos/Guitar-min.png";
-import HandBag from "../../assets/photos/HandBag-min.png";
-import Headphones from "../../assets/photos/Headphones-min.png";
-import Hospitals from "../../assets/photos/Hospitals-min.png";
-import Hotel from "../../assets/photos/Hotel-min.png";
+// import Flute from "../../assets/photos/Flute-min.png";
+// import Footwear from "../../assets/photos/Footwear-min.png";
+// import Furniture from "../../assets/photos/Furniture-min.png";
+// import Guitar from "../../assets/photos/Guitar-min.png";
+// import HandBag from "../../assets/photos/HandBag-min.png";
+// import Headphones from "../../assets/photos/Headphones-min.png";
+// import Hospitals from "../../assets/photos/Hospitals-min.png";
+// import Hotel from "../../assets/photos/Hotel-min.png";
 
-import IceCream from "../../assets/photos/IceCream-min.png";
-import InsuranceCompany from "../../assets/photos/InsuranceCompany-min.png";
-import InteriorDesigners from "../../assets/photos/InteriorDesigners-min.png";
-import InvestmentFirms from "../../assets/photos/InvestmentFirms-min.png";
-import Jewellaries from "../../assets/photos/Jewellaries-min.png";
-import Laptop from "../../assets/photos/Laptop-min.png";
-import Luggage from "../../assets/photos/Luggage-min.png";
-import Malls from "../../assets/photos/Malls-min.png";
+// import IceCream from "../../assets/photos/IceCream-min.png";
+// import InsuranceCompany from "../../assets/photos/InsuranceCompany-min.png";
+// import InteriorDesigners from "../../assets/photos/InteriorDesigners-min.png";
+// import InvestmentFirms from "../../assets/photos/InvestmentFirms-min.png";
+// import Jewellaries from "../../assets/photos/Jewellaries-min.png";
+// import Laptop from "../../assets/photos/Laptop-min.png";
+// import Luggage from "../../assets/photos/Luggage-min.png";
+// import Malls from "../../assets/photos/Malls-min.png";
 
-import Mithai from "../../assets/photos/Mithai-min.png";
-import Mobile from "../../assets/photos/Mobile-min.png";
-import Motorbike from "../../assets/photos/Motorbike-min.png";
-import Movies from "../../assets/photos/Movies-min.png";
-import Museum from "../../assets/photos/Museum-min.png";
-import MusicFest from "../../assets/photos/MusicFest-min.png";
-import Newspaper from "../../assets/photos/Newspaper-min.png";
-import NGOs from "../../assets/photos/NGOs-min.png";
+// import Mithai from "../../assets/photos/Mithai-min.png";
+// import Mobile from "../../assets/photos/Mobile-min.png";
+// import Motorbike from "../../assets/photos/Motorbike-min.png";
+// import Movies from "../../assets/photos/Movies-min.png";
+// import Museum from "../../assets/photos/Museum-min.png";
+// import MusicFest from "../../assets/photos/MusicFest-min.png";
+// import Newspaper from "../../assets/photos/Newspaper-min.png";
+// import NGOs from "../../assets/photos/NGOs-min.png";
 
-import Notebooks from "../../assets/photos/Notebooks-min.png";
-import Paint from "../../assets/photos/Paint-min.png";
-import Park from "../../assets/photos/Park-min.png";
-import Pencil from "../../assets/photos/Pencil-min.png";
-import Pen from "../../assets/photos/Pen-min.png";
-import Perfume from "../../assets/photos/Perfume-min.png";
-import PhotoStudio from "../../assets/photos/PhotoStudio-min.png";
-import Piano from "../../assets/photos/Piano-min.png";
+// import Notebooks from "../../assets/photos/Notebooks-min.png";
+// import Paint from "../../assets/photos/Paint-min.png";
+// import Park from "../../assets/photos/Park-min.png";
+// import Pencil from "../../assets/photos/Pencil-min.png";
+// import Pen from "../../assets/photos/Pen-min.png";
+// import Perfume from "../../assets/photos/Perfume-min.png";
+// import PhotoStudio from "../../assets/photos/PhotoStudio-min.png";
+// import Piano from "../../assets/photos/Piano-min.png";
 
-import Restaurant from "../../assets/photos/Restaurant-min.png";
-import Salon from "../../assets/photos/Salon-min.png";
-import School from "../../assets/photos/School-min.png";
-import Scooter from "../../assets/photos/Scooter-min.png";
-import Shampoo from "../../assets/photos/Shampoo-min.png";
-import Showpiece from "../../assets/photos/Showpiece-min.png";
-import Soap from "../../assets/photos/Soap-min.png";
-import Songs from "../../assets/photos/Songs-min.png";
+// import Restaurant from "../../assets/photos/Restaurant-min.png";
+// import Salon from "../../assets/photos/Salon-min.png";
+// import School from "../../assets/photos/School-min.png";
+// import Scooter from "../../assets/photos/Scooter-min.png";
+// import Shampoo from "../../assets/photos/Shampoo-min.png";
+// import Showpiece from "../../assets/photos/Showpiece-min.png";
+// import Soap from "../../assets/photos/Soap-min.png";
+// import Songs from "../../assets/photos/Songs-min.png";
 
-import Spa from "../../assets/photos/Spa-min.png";
-import Speaker from "../../assets/photos/Speaker-min.png";
-import Sunglasses from "../../assets/photos/Sunglasses-min.png";
-import Tea from "../../assets/photos/Tea-min.png";
-import Tech from "../../assets/photos/Tech-min.png";
-import Toohbrush from "../../assets/photos/Toohbrush-min.png";
-import Toothpaste from "../../assets/photos/Toothpaste-min.png";
-import Watch from "../../assets/photos/Watch-min.png";
+// import Spa from "../../assets/photos/Spa-min.png";
+// import Speaker from "../../assets/photos/Speaker-min.png";
+// import Sunglasses from "../../assets/photos/Sunglasses-min.png";
+// import Tea from "../../assets/photos/Tea-min.png";
+// import Tech from "../../assets/photos/Tech-min.png";
+// import Toohbrush from "../../assets/photos/Toohbrush-min.png";
+// import Toothpaste from "../../assets/photos/Toothpaste-min.png";
+// import Watch from "../../assets/photos/Watch-min.png";
 
-import Wine from "../../assets/photos/Wine-min.png";
-import Zoo from "../../assets/photos/Zoo-min.png";
+// import Wine from "../../assets/photos/Wine-min.png";
+// import Zoo from "../../assets/photos/Zoo-min.png";
 
-export const MobileViewImages = [
-  {
-    image: Perfume,
-    name: "Perfume",
-  },
-  {
-    image: Banks,
-    name: "Banks",
-  },
+// export const MobileViewImages = [
+//   {
+//     image: Perfume,
+//     name: "Perfume",
+//   },
+//   {
+//     image: Banks,
+//     name: "Banks",
+//   },
 
-  {
-    image: Car,
-    name: "Car",
-  },
-  {
-    image: PhotoStudio,
-    name: "PhotoStudio",
-  },
+//   {
+//     image: Car,
+//     name: "Car",
+//   },
+//   {
+//     image: PhotoStudio,
+//     name: "PhotoStudio",
+//   },
 
-  {
-    image: DrumKit,
-    name: "DrumKit",
-  },
+//   {
+//     image: DrumKit,
+//     name: "DrumKit",
+//   },
 
-  {
-    image: Headphones,
-    name: "Headphones",
-  },
+//   {
+//     image: Headphones,
+//     name: "Headphones",
+//   },
 
-  {
-    image: Mithai,
-    name: "Mithai",
-  },
-  {
-    image: Candy,
-    name: "Candy",
-  },
+//   {
+//     image: Mithai,
+//     name: "Mithai",
+//   },
+//   {
+//     image: Candy,
+//     name: "Candy",
+//   },
 
-  {
-    image: Chips,
-    name: "Chips",
-  },
-  {
-    image: ColdDrinks,
-    name: "ColdDrinks",
-  },
+//   {
+//     image: Chips,
+//     name: "Chips",
+//   },
+//   {
+//     image: ColdDrinks,
+//     name: "ColdDrinks",
+//   },
 
-  {
-    image: Laptop,
-    name: "Laptop",
-  },
-  {
-    image: Hospitals,
-    name: "Hospitals",
-  },
+//   {
+//     image: Laptop,
+//     name: "Laptop",
+//   },
+//   {
+//     image: Hospitals,
+//     name: "Hospitals",
+//   },
 
-  {
-    image: Luggage,
-    name: "Luggage",
-  },
+//   {
+//     image: Luggage,
+//     name: "Luggage",
+//   },
 
-  {
-    image: Pen,
-    name: "Pen",
-  },
+//   {
+//     image: Pen,
+//     name: "Pen",
+//   },
 
-  {
-    image: Notebooks,
-    name: "Notebooks",
-  },
-  {
-    image: Newspaper,
-    name: "Newspaper",
-  },
+//   {
+//     image: Notebooks,
+//     name: "Notebooks",
+//   },
+//   {
+//     image: Newspaper,
+//     name: "Newspaper",
+//   },
 
-  {
-    image: Showpiece,
-    name: "Showpiece",
-  },
-  {
-    image: Speaker,
-    name: "Speaker",
-  },
+//   {
+//     image: Showpiece,
+//     name: "Showpiece",
+//   },
+//   {
+//     image: Speaker,
+//     name: "Speaker",
+//   },
 
-  {
-    image: Sunglasses,
-    name: "Sunglasses",
-  },
-  {
-    image: Shampoo,
-    name: "Shampoo",
-  },
+//   {
+//     image: Sunglasses,
+//     name: "Sunglasses",
+//   },
+//   {
+//     image: Shampoo,
+//     name: "Shampoo",
+//   },
 
-  {
-    image: Soap,
-    name: "Soap",
-  },
+//   {
+//     image: Soap,
+//     name: "Soap",
+//   },
 
-  {
-    image: Aquarium,
-    name: "Aquarium",
-  },
+//   {
+//     image: Aquarium,
+//     name: "Aquarium",
+//   },
 
-  {
-    image: Beach,
-    name: "Beach",
-  },
-  {
-    image: Museum,
-    name: "Museum",
-  },
+//   {
+//     image: Beach,
+//     name: "Beach",
+//   },
+//   {
+//     image: Museum,
+//     name: "Museum",
+//   },
 
-  {
-    image: Camera,
-    name: "Camera",
-  },
-  {
-    image: Decorators,
-    name: "Decorators",
-  },
+//   {
+//     image: Camera,
+//     name: "Camera",
+//   },
+//   {
+//     image: Decorators,
+//     name: "Decorators",
+//   },
 
-  {
-    image: Park,
-    name: "Park",
-  },
-  {
-    image: Tea,
-    name: "Tea",
-  },
+//   {
+//     image: Park,
+//     name: "Park",
+//   },
+//   {
+//     image: Tea,
+//     name: "Tea",
+//   },
 
-  {
-    image: Biscuit,
-    name: "Biscuit",
-  },
+//   {
+//     image: Biscuit,
+//     name: "Biscuit",
+//   },
 
-  {
-    image: Toothpaste,
-    name: "Toothpaste",
-  },
+//   {
+//     image: Toothpaste,
+//     name: "Toothpaste",
+//   },
 
-  {
-    image: Zoo,
-    name: "Zoo",
-  },
+//   {
+//     image: Zoo,
+//     name: "Zoo",
+//   },
 
-  {
-    image: Songs,
-    name: "Songs",
-  },
-  {
-    image: Toohbrush,
-    name: "Toohbrush",
-  },
-  {
-    image: Flute,
-    name: "Flute",
-  },
-  {
-    image: Pencil,
-    name: "Pencil",
-  },
+//   {
+//     image: Songs,
+//     name: "Songs",
+//   },
+//   {
+//     image: Toohbrush,
+//     name: "Toohbrush",
+//   },
+//   {
+//     image: Flute,
+//     name: "Flute",
+//   },
+//   {
+//     image: Pencil,
+//     name: "Pencil",
+//   },
 
-  {
-    image: Tech,
-    name: "Tech",
-  },
-  {
-    image: Movies,
-    name: "Movies",
-  },
+//   {
+//     image: Tech,
+//     name: "Tech",
+//   },
+//   {
+//     image: Movies,
+//     name: "Movies",
+//   },
 
-  {
-    image: Books,
-    name: "Books",
-  },
-  {
-    image: Clothes,
-    name: "Clothes",
-  },
+//   {
+//     image: Books,
+//     name: "Books",
+//   },
+//   {
+//     image: Clothes,
+//     name: "Clothes",
+//   },
 
-  {
-    image: EventOrganizer,
-    name: "EventOrganizer",
-  },
-  {
-    image: Apps,
-    name: "Apps",
-  },
+//   {
+//     image: EventOrganizer,
+//     name: "EventOrganizer",
+//   },
+//   {
+//     image: Apps,
+//     name: "Apps",
+//   },
 
-  {
-    image: Hotel,
-    name: "Hotel",
-  },
-  {
-    image: InteriorDesigners,
-    name: "InteriorDesigners",
-  },
+//   {
+//     image: Hotel,
+//     name: "Hotel",
+//   },
+//   {
+//     image: InteriorDesigners,
+//     name: "InteriorDesigners",
+//   },
 
-  {
-    image: IceCream,
-    name: "IceCream",
-  },
-  {
-    image: Spa,
-    name: "Spa",
-  },
-  {
-    image: InvestmentFirms,
-    name: "InvestmentFirms",
-  },
+//   {
+//     image: IceCream,
+//     name: "IceCream",
+//   },
+//   {
+//     image: Spa,
+//     name: "Spa",
+//   },
+//   {
+//     image: InvestmentFirms,
+//     name: "InvestmentFirms",
+//   },
 
-  {
-    image: Guitar,
-    name: "Guitar",
-  },
+//   {
+//     image: Guitar,
+//     name: "Guitar",
+//   },
 
-  {
-    image: NGOs,
-    name: "NGOs",
-  },
+//   {
+//     image: NGOs,
+//     name: "NGOs",
+//   },
 
-  {
-    image: AmusementPark,
-    name: "AmusementPark",
-  },
-  {
-    image: MusicFest,
-    name: "MusicFest",
-  },
+//   {
+//     image: AmusementPark,
+//     name: "AmusementPark",
+//   },
+//   {
+//     image: MusicFest,
+//     name: "MusicFest",
+//   },
 
-  {
-    image: Bakeries,
-    name: "Bakeries",
-  },
-  {
-    image: Restaurant,
-    name: "Restaurant",
-  },
-  {
-    image: Cosmetics,
-    name: "Cosmetics",
-  },
-  {
-    image: HandBag,
-    name: "HandBag",
-  },
-  {
-    image: College,
-    name: "College",
-  },
+//   {
+//     image: Bakeries,
+//     name: "Bakeries",
+//   },
+//   {
+//     image: Restaurant,
+//     name: "Restaurant",
+//   },
+//   {
+//     image: Cosmetics,
+//     name: "Cosmetics",
+//   },
+//   {
+//     image: HandBag,
+//     name: "HandBag",
+//   },
+//   {
+//     image: College,
+//     name: "College",
+//   },
 
-  {
-    image: CutlerySet,
-    name: "CutlerySet",
-  },
+//   {
+//     image: CutlerySet,
+//     name: "CutlerySet",
+//   },
 
-  {
-    image: Furniture,
-    name: "Furniture",
-  },
-  {
-    image: Jewellaries,
-    name: "Jewellaries",
-  },
+//   {
+//     image: Furniture,
+//     name: "Furniture",
+//   },
+//   {
+//     image: Jewellaries,
+//     name: "Jewellaries",
+//   },
 
-  {
-    image: School,
-    name: "School",
-  },
-  {
-    image: InsuranceCompany,
-    name: "InsuranceCompany",
-  },
+//   {
+//     image: School,
+//     name: "School",
+//   },
+//   {
+//     image: InsuranceCompany,
+//     name: "InsuranceCompany",
+//   },
 
-  {
-    image: Piano,
-    name: "Piano",
-  },
-  {
-    image: Scooter,
-    name: "Scooter",
-  },
+//   {
+//     image: Piano,
+//     name: "Piano",
+//   },
+//   {
+//     image: Scooter,
+//     name: "Scooter",
+//   },
 
-  {
-    image: Motorbike,
-    name: "Motorbike",
-  },
+//   {
+//     image: Motorbike,
+//     name: "Motorbike",
+//   },
 
-  {
-    image: Bicycle,
-    name: "Bicycle",
-  },
+//   {
+//     image: Bicycle,
+//     name: "Bicycle",
+//   },
 
-  {
-    image: Salon,
-    name: "Salon",
-  },
-  {
-    image: Wine,
-    name: "Wine",
-  },
+//   {
+//     image: Salon,
+//     name: "Salon",
+//   },
+//   {
+//     image: Wine,
+//     name: "Wine",
+//   },
 
-  {
-    image: Coffee,
-    name: "Coffee",
-  },
-  {
-    image: Beer,
-    name: "Beer",
-  },
+//   {
+//     image: Coffee,
+//     name: "Coffee",
+//   },
+//   {
+//     image: Beer,
+//     name: "Beer",
+//   },
 
-  {
-    image: Paint,
-    name: "Paint",
-  },
-  {
-    image: Footwear,
-    name: "Footwear",
-  },
+//   {
+//     image: Paint,
+//     name: "Paint",
+//   },
+//   {
+//     image: Footwear,
+//     name: "Footwear",
+//   },
 
-  {
-    image: Mobile,
-    name: "Mobile",
-  },
+//   {
+//     image: Mobile,
+//     name: "Mobile",
+//   },
 
-  {
-    image: Watch,
-    name: "Watch",
-  },
+//   {
+//     image: Watch,
+//     name: "Watch",
+//   },
 
-  {
-    image: Malls,
-    name: "Malls",
-  },
-  {
-    image: Cruise,
-    name: "Cruise",
-  },
-];
+//   {
+//     image: Malls,
+//     name: "Malls",
+//   },
+//   {
+//     image: Cruise,
+//     name: "Cruise",
+//   },
+// ];
 
 
